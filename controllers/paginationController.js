423b024f1dc24b37d96eab4d7caba97cf5683fe1
B0ySetsF1re <montersberg@gmail.com>
2020-08-27 11:44:21 +0300

    configured lorem-ipsum middleware in pagination controller

--- controllers/paginationController.js
+++ controllers/paginationController.js
@@ -1,2 +1,14 @@
 const mongojs = require('mongojs');
 var db = mongojs('pagination_examples', ['example_a', 'example_b', 'example_c']);
+
+const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4
+  }
+});
